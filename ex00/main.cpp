b0173cfe25973420c809bfa423a5e96b8be088cf
fheld <fheld@student.42berlin.de>
2023-10-22 00:53:52 +0200

    wrote program that prints string in uppercase

---
+++ ex00/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	if(argc != 2) std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else {std::string input; input = argv[1];
+		for (std::string::iterator i = input.begin(); i < input.end(); i++)
+			{std::cout << (char) std::toupper(*i);}
+		std::cout << std::endl;}
+}
